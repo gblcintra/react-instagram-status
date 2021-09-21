923f3d2186748a26b7c5019d2cae3e57f848e927
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-21 12:20:56 -0300

    feat(old version): test old version(21092021)

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,33 @@ let photos: PhotoProps[] = [
 	}
 ];
 
+function renderLi(item: PhotoProps) {
+	const items = []
+	for (let i = 0; item.qtd > i; i++) {
+		items.push(<li className='color'></li>)
+	};
+	return items
+}
 
 function App() {
 	return (
 		<div className="container">
 			{photos.map((item, index) => (
-			<div className="circle" key={index}>
-				<img src={item.url} alt={item.name} />
-				<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">  
-					<circle cx="50" cy="50" r="40" />
-				</svg>
-			</div>
+				<div key={index}>
+					<div className="justify-content-center">
+						<div className="image_outer_container">
+							{/* <div className="green_icon"></div> */}
+							<div className="image_inner_container">
+								<img src={item.url} alt={item.name} />
+							</div>
+						</div>
+					</div>
+					<div className="wheel">
+						<ul className="umbrella" id='teste2'>
+							{renderLi(item)}
+						</ul>
+					</div>
+				</div>
 			))}
 		</div>
 	);
