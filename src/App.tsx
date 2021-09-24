1583c37d69e889b390b109c0918806f48a55c5eb
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-24 14:53:57 -0300

    feat(border): adjust margin border (24092021)

--- src/App.tsx
+++ src/App.tsx
@@ -26,44 +26,79 @@ let photos: PhotoProps[] = [
 		url: 'https://avatars.githubusercontent.com/u/15270961?v=4',
 		name: 'photo',
 		qtd: 5
+	},
+	{
+		url: 'https://avatars.githubusercontent.com/u/15270961?v=4',
+		name: 'photo',
+		qtd: 6
+	},
+	{
+		url: 'https://avatars.githubusercontent.com/u/15270961?v=4',
+		name: 'photo',
+		qtd: 7
 	}
 ];
 
 const positionStatus: PositionStatusProps[] = [
 	{
 		qtd: 1,
-		position: 0
+		dasharray: 0,
+		dashoffsetOne: 0,
+		dashoffsetTwo: 0,
 	},
 	{
 		qtd: 2,
-		position: 63
+		dasharray: 63,
+		dashoffsetOne: -3,
+		dashoffsetTwo: -60,
 	},
 	{
 		qtd: 3,
-		position: 42
+		dasharray: 42,
+		dashoffsetOne: -17,
+		dashoffsetTwo: 20,
 	},
 	{
 		qtd: 4,
-		position: 32
+		dasharray: 32,
+		dashoffsetOne: -31,
+		dashoffsetTwo: 6,
 	},
 	{
 		qtd: 5,
-		position: 25
+		dasharray: 25,
+		dashoffsetOne: 9,
+		dashoffsetTwo: -10,
 	},
 	{
 		qtd: 6,
-		position: 21
+		dasharray: 21,
+		dashoffsetOne: -17,
+		dashoffsetTwo: -1,
 	},
 	{
 		qtd: 7,
-		position: 16
+		dasharray: 16,
+		dashoffsetOne: -8,
+		dashoffsetTwo: 3,
 	}
 ]
 
 
-function teste(qtd: number){
-	const position = positionStatus.find( element => element.qtd === qtd) 
-	return position ? position.position : 0;
+function teste(qtd: number, index: number) {
+	const position = positionStatus.find(element => element.qtd === qtd)
+	const style =
+		`
+		.classSVG${index}{
+			stroke-dasharray: ${position ? position.dasharray : 0}!important;
+			stroke-dashoffset: ${position ? position.dashoffsetOne : 0}!important;
+		}
+		.classSVGb${index}{
+			stroke-dasharray: ${position ? position.dasharray : 0}!important;
+			stroke-dashoffset: ${position ? position.dashoffsetTwo : 0}!important;
+		}
+		`
+	return style;
 }
 
 function App() {
@@ -73,11 +108,12 @@ function App() {
 				<div className="circle" key={index}>
 					<img src={item.url} alt={item.name} />
 					<style>
-						{`.classSVG${index}{
-							stroke-dasharray: ${teste(item.qtd)}!important
-						}`}
+						{teste(item.qtd, index)}
 					</style>
-					<svg className={'classSVG'+index} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+					<svg className={'svg1 classSVG'+index} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+						<circle cx="50" cy="50" r="40" />
+					</svg>
+					<svg className={'svg2 classSVGb'+index} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 						<circle cx="50" cy="50" r="40" />
 					</svg>
 				</div>
