d4e47e96c7f64d61538b67939f31dbc251655e11
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2021-09-24 15:25:38 -0300

    feat(general): adjust repo (24092021)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
-import { PhotoProps, PositionStatusProps } from './interface'
-
+import { PhotoProps } from './interface'
+import PositionPhoto from './components/Position' 
 let photos: PhotoProps[] = [
 	{
 		url: 'https://avatars.githubusercontent.com/u/15270961?v=4',
@@ -39,68 +39,6 @@ let photos: PhotoProps[] = [
 	}
 ];
 
-const positionStatus: PositionStatusProps[] = [
-	{
-		qtd: 1,
-		dasharray: 0,
-		dashoffsetOne: 0,
-		dashoffsetTwo: 0,
-	},
-	{
-		qtd: 2,
-		dasharray: 63,
-		dashoffsetOne: -3,
-		dashoffsetTwo: -60,
-	},
-	{
-		qtd: 3,
-		dasharray: 42,
-		dashoffsetOne: -17,
-		dashoffsetTwo: 20,
-	},
-	{
-		qtd: 4,
-		dasharray: 32,
-		dashoffsetOne: -31,
-		dashoffsetTwo: 6,
-	},
-	{
-		qtd: 5,
-		dasharray: 25,
-		dashoffsetOne: 9,
-		dashoffsetTwo: -10,
-	},
-	{
-		qtd: 6,
-		dasharray: 21,
-		dashoffsetOne: -17,
-		dashoffsetTwo: -1,
-	},
-	{
-		qtd: 7,
-		dasharray: 16,
-		dashoffsetOne: -8,
-		dashoffsetTwo: 3,
-	}
-]
-
-
-function teste(qtd: number, index: number) {
-	const position = positionStatus.find(element => element.qtd === qtd)
-	const style =
-		`
-		.classSVG${index}{
-			stroke-dasharray: ${position ? position.dasharray : 0}!important;
-			stroke-dashoffset: ${position ? position.dashoffsetOne : 0}!important;
-		}
-		.classSVGb${index}{
-			stroke-dasharray: ${position ? position.dasharray : 0}!important;
-			stroke-dashoffset: ${position ? position.dashoffsetTwo : 0}!important;
-		}
-		`
-	return style;
-}
-
 function App() {
 	return (
 		<div className="container">
@@ -108,7 +46,7 @@ function App() {
 				<div className="circle" key={index}>
 					<img src={item.url} alt={item.name} />
 					<style>
-						{teste(item.qtd, index)}
+						{PositionPhoto(item.qtd, index)}
 					</style>
 					<svg className={'svg1 classSVG'+index} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
 						<circle cx="50" cy="50" r="40" />
